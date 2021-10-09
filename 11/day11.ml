bce56da6edbb8249f9a6393213f4bbd5961dbedc
Ravi Chandra <rc@braveface.nz>
2021-10-09 19:59:05 +1300

    need to generate all combinations

--- 11/day11.ml
+++ 11/day11.ml
@@ -8,6 +8,12 @@ type move = Down of element list | Up of element list [@@deriving show]
 
 type st = element list array * int [@@deriving show, eq, ord]
 
+module State = struct
+  type t = st
+
+  let compare = compare_st
+end
+
 let initial =
   ( [|
       [ Chip "hydrogen"; Chip "lithium" ];
@@ -18,6 +24,17 @@ let initial =
     0 )
 
 module StringSet = Set.Make (String)
+module StateSet = Set.Make (State)
+
+let print_state (data, floor_idx) =
+  Array.iteri
+    (fun i lst ->
+      Printf.printf "%cF%d - %s\n"
+        (if i = floor_idx then '*' else ' ')
+        (i + 1)
+        (String.concat " " (List.map show_element lst)))
+    data;
+  print_endline ""
 
 let bad_floor lst =
   let chips, gens =
@@ -45,7 +62,7 @@ let find_moves lst =
         (fun acc el -> Up [ Chip el; Generator el ] :: acc)
         [] (StringSet.to_list inter)
     in
-    (* then all chips *)
+    (* then all chips individually *)
     List.fold_left
       (fun acc el -> Up [ Chip el ] :: Down [ Chip el ] :: acc)
       acc (StringSet.to_list chips')
@@ -55,12 +72,12 @@ let find_moves lst =
      acc' (StringSet.to_list rem_gens) *)
 
 let make_candidate state floor_idx move =
-  (* Printf.printf "MOVE %s\n" (show_move move); *)
+  Printf.printf "MOVE %s\n" (show_move move);
   let floor = state.(floor_idx) in
   let new_state = Array.copy state in
   match move with
   | Up els ->
-      if floor_idx = 4 then None
+      if floor_idx = 3 then None
       else
         let up_floor = els @ state.(floor_idx + 1) in
         let curr_floor =
@@ -84,7 +101,7 @@ let make_candidate state floor_idx move =
         new_state.(floor_idx - 1) <- down_floor;
         Some (new_state, floor_idx - 1)
 
-let candidates (state, floor_idx) =
+let find_candidates (state, floor_idx) =
   let floor = state.(floor_idx) in
   List.map (fun move -> make_candidate state floor_idx move) (find_moves floor)
   |> List.keep_some
@@ -103,21 +120,47 @@ let valid_state (state, _) = Array.filter bad_floor state |> Array.length = 0
    in
    aux 0 initial_state *)
 
+(* let process initial_state =
+   let rec aux steps seen = function
+     | st :: xs ->
+         if StateSet.mem st seen then aux steps seen xs
+         else (
+           Printf.printf "step (%d)\n%s\nvalid - %B\n" steps (show_st st) (valid_state st);
+           let seen' = StateSet.add st seen in
+           let data, floor_idx = st in
+           if floor_idx = 3 && List.length data.(floor_idx) = 4 then (
+             print_endline "FOUND/?!";
+             Some (steps, st))
+           else
+             let new_candidates = find_candidates st |> List.filter valid_state in
+             aux (steps + 1) seen' (new_candidates @ xs))
+     | [] -> None
+   in
+   aux 0 StateSet.empty [ initial_state ] *)
+
 let process initial_state =
-  let rec aux steps next_states =
-    match next_states with
-    | [] -> failwith "what?"
-    | st :: xs ->
+  let rec aux steps seen q =
+    if CCSimple_queue.is_empty q then failwith "not found?"
+    else
+      let st, q' = CCSimple_queue.pop_exn q in
+      if StateSet.mem st seen then aux steps seen q'
+      else (
+        Printf.printf "step (%d) valid - %B\n" steps (valid_state st);
+        print_state st;
+        let seen' = StateSet.add st seen in
         let data, floor_idx = st in
-        if floor_idx = 4 && List.length data.(floor_idx) = 4 then (
+        if floor_idx = 3 && List.length data.(floor_idx) = 4 then (
           print_endline "FOUND/?!";
           (steps, st))
         else
-          let valid_candidates = candidates st |> List.filter valid_state in
-          aux (steps + 1) (xs @ valid_candidates)
-    (* List.fold_while (fun acc ns -> (aux (acc + 1) ns, `Stop)) acc next_states *)
+          let new_candidates = find_candidates st |> List.filter valid_state in
+          let q'' =
+            List.fold_left (fun acc c -> CCSimple_queue.push c acc) q' new_candidates
+          in
+          aux (steps + 1) seen' q'')
   in
-  aux 0 [ initial_state ]
+
+  aux 0 StateSet.empty CCSimple_queue.(empty |> push initial_state)
 
 let () =
   (* let data = Aoc.read_lines "input.txt" |> List.map String.trim in
