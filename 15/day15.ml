5884791d03d5bc33f4856012cfd151f2e00505f2
Ravi Chandra <rc@braveface.nz>
2021-10-16 14:18:10 +1300

    day 15, with part 2

--- 15/day15.ml
+++ 15/day15.ml
@@ -2,17 +2,21 @@ open Containers
 open Format
 open Aoc
 
-let data = [ (5, 2); (13, 7); (17, 10); (3, 2); (19, 9); (7, 0) ]
+let data1 = [ (5, 2); (13, 7); (17, 10); (3, 2); (19, 9); (7, 0) ]
+
+let data2 = [ (5, 2); (13, 7); (17, 10); (3, 2); (19, 9); (7, 0); (11, 0) ]
 
 let solve disc_num start_pos num_pos t =
   rem (start_pos + t) num_pos = rem (num_pos - disc_num) num_pos
 
-let solve_eqs t = List.mapi (fun i (p, s) -> solve (i + 1) s p t) data
+let solve_eqs data t = List.mapi (fun i (p, s) -> solve (i + 1) s p t) data
 
-let solve =
+let solve data =
   Iter.init Aoc.identity
-  |> Iter.map (fun i -> (i, solve_eqs i))
+  |> Iter.map (fun i -> (i, solve_eqs data i))
   |> Iter.filter (fun (_, result) -> List.for_all Aoc.identity result)
   |> Iter.head_exn
 
-let () = printf "%d\n" (fst solve)
+let () =
+  printf "part1 = %d\n" (fst (solve data1));
+  printf "part2 = %d\n" (fst (solve data2))
